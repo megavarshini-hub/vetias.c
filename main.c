758e841f6c361db9685a50b66104aa44af774526
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-12 13:58:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a= 5; //defintion
+    int b; //declaration
+    b = 5; //initialization
+    int sum=a+b;
+    printf("sum = %d", sum);
+    
+    ;
+     return 0;
+}
