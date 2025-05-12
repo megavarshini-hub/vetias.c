c531fe06b38b009be6ce0a3a2490e008cb1d980b
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-12 16:02:08 +0530

    Create wsap.c

---
+++ wsap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a= 5; //defintion
+    int b; //declaration
+    b = 4; //initialization
+    int temp =a;
+
+    printf("%d",a);
+printf("%d", b);
+     return 0;
+}
