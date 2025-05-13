c0057f2c487b61159c36d339944c2309337bc041
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-13 14:00:08 +0530

    Create oddeve.c

---
+++ oddeve.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%2) 
+    {
+        printf("the given is odd:");
+    }
+     else
+    {
+        printf("the given is even:");
+             
+    }
+        return 0;
+    
+}
+    
+    
