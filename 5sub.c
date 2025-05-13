d4d4e33524c32436dd093a980e97482c97dc7dcf
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-13 14:24:07 +0530

    Create 5sub.c

---
+++ 5sub.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int s1,s2,s3,s4,s5,total,avg;
+    printf("enter the mark of s1:");
+
+     scanf("%d",&s1);
+     printf("enter the mark of s2:");
+      scanf("%d",&s2);
+printf("enter the mark of s3:");
+ scanf("%d",&s3);
+printf("enter the mark of s4:");
+ scanf("%d",&s4);
+printf("enter the mark of s5:");
+ scanf("%d",&s5);
+ total= s1+s2+s3+s4+s5;
+ printf("total mark is  %d",total);
+ avg= total/5;
+ printf("avg is %d",avg);
+
+    return 0;
+}
