ec687e004393e71084f61e5225487d4be9b210b8
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-13 14:05:47 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%400==0) 
+    {
+        printf("the given is leapyear:");
+    }
+     else
+    {
+        printf("the given is not leap year:");
+             
+             
+    }
+        return 0;
+    
+}
+    
+    
