c575aad05bf2b451dab2dfbfb419ab5447a75408
megavarshini-hub <megavarshinir23csa@vetias.ac.in>
2025-05-15 15:41:15 +0530

    Create reverse string.c

---
+++ reverse string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int num;
+    printf("enter the integer:");
+    scanf ("%d", &num);
+    while(num>0)
+    {
+        int digit=num%10;
+        num=num/10;
+        printf("%d", digit);
+    }
+    
+
+    return 0;
+}
